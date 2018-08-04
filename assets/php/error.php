d763eb0eab3ef412fd664ed570902dadf19c45f8
Ted Noelker <tnoelker@redventures.com>
2018-08-04 16:12:44 -0400

    Wipe clean

--- assets/php/error.php
+++
@@ -1,7 +0,0 @@
-<main class="wide">
-    <section class="error gutter">
-        <h1><?php echo $this->error['display']; ?></h1>
-        <h2><?php echo $this->error['number']; ?> error</h2>
-        <p><?php echo $this->error['message']; ?></p>
-    </section>
-</main>
