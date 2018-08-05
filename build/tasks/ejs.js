22d1393ccf42891355ad2e054b4c30b14ca449f8
Ted Noelker <tnoelker@redventures.com>
2018-08-04 21:22:57 -0400

    POC static generator

--- build/tasks/ejs.js
+++ build/tasks/ejs.js
@@ -6,7 +6,7 @@ const gulp = require('gulp');
 module.exports = (config, data) => {
   return getPages(config).forEach(file => {
     gulp
-      .src(`${config.get('template')}/${file.template}`)
+      .src(`${config.get('template')}/${file.template}.ejs`)
       .pipe(ejs(
           { page: file.content },
           { rmWhitespace: true },
@@ -21,7 +21,9 @@ module.exports = (config, data) => {
 function getPages(config, pages = []) {
   fs.readdirSync(config.get('pages')).forEach(fileName => {
     const pageName = fileName.replace('.json', '');
-    const filePath = `${config.get('pages')/fileName}`;
+    const filePath = `${config.get('pages')}/${fileName}`;
+    console.log(filePath);
+    console.log(require(filePath));
     delete require.cache[require.resolve(filePath)];
     if (typeof require(filePath).template === 'string') {
       pages.push({
