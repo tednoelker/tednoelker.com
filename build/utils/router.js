fd8accdbe4034db9ce5379229869149f29e4aee5
Ted Noelker <ted@noelker.info>
2019-07-20 22:07:26 -0400

    Update conditional logic

--- build/utils/router.js
+++ build/utils/router.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // Default to html extension if not specified
 module.exports = directory => (req, res, next) => {
-  if (req.method !== 'GET' && req.method !== 'HEAD' && req.url !== '/' && path.extname(req.url) === '') {
+  if ((req.method === 'GET' || req.method === 'HEAD') && req.url !== '/' && path.extname(req.url) === '') {
     fs.exists(`${directory}${req.url}.html`, (exists) => {
       if (exists) {
         req.url += '.html';
