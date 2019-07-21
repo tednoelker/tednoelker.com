d6f1964b30edacf444754f693be193dde979105e
Ted Noelker <ted@noelker.info>
2019-07-20 22:02:49 -0400

    Remove html file extension

---
+++ build/utils/router.js
@@ -0,0 +1,20 @@
+// Dependencies
+const fs = require('fs');
+const path = require('path');
+
+// Default to html extension if not specified
+module.exports = directory => (req, res, next) => {
+	if (req.method !== 'GET' && req.method !== 'HEAD') {
+		return next();
+	}
+	if (req.url !== '/' && path.extname(req.url) === '') {
+		fs.exists(`${directory}${req.url}.html`, (exists) => {
+			if (exists) {
+				req.url += '.html';
+			}
+			next();
+		});
+	} else {
+		next();
+	}
+};
